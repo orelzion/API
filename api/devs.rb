b749051eadfa728c94ec04d099158852e8f9aa26
orelzion <orel.z@tvinci.com>
2014-09-03 16:08:38 +0300

    completing dev class, added order and sort to get, fixing the put

--- api/devs.rb
+++ api/devs.rb
@@ -3,10 +3,24 @@ require 'grape'
 class Devs < Grape::API
 	resource :devs do
 
-		desc "Get all devs"
-		get do
+		before do 
 			authenticate!
-			Dev.all
+			if(current_dev.access != :all) then 
+				error!('You have no right to access devs DB', 403) 
+			end
+		end
+
+		desc "Get all devs"
+		params do 
+			optional :order_by, type: Symbol, values: [:dev_name, :created_at, :updated_at, :email, :id], default: :id
+			optional :sort_by,  type: Symbol, values: [:asc, :desc], default: :asc
+		end
+		get do
+			if(params[:sort_by] == :asc) then
+				Dev.all(:order => params[:order_by].asc)
+			else
+				Dev.all(:order => params[:order_by].desc)
+			end
 		end
 
 		namespace :email do
@@ -15,7 +29,6 @@ class Devs < Grape::API
 	    		requires :email, type: String, desc: "Dev's email"
 	  		end
 				get ':email', requirements: { email: /.*/ } do
-				authenticate!
 				Dev.all(:email => params[:email])
 			end
 		end
@@ -26,19 +39,12 @@ class Devs < Grape::API
 	    		requires :key, type: String, desc: "Dev's api key"
 	  		end
 				get ':key' do
-				authenticate!
-				logger.info("api key is #{params[:key]}")
 				Dev.first(:api_key => params[:key])
 			end
 		end
 
 		desc 'Create a new dev'
 		post do 
-			authenticate!
-			if(current_dev.access == :read ||
-				current_dev.access == :modify) then
-				error!('You have no rights to create a new dev', 403)
-			end
 			@dev = Dev.new
 			@dev.email = params[:email]
 			@dev.dev_name = params[:dev_name]
@@ -53,16 +59,13 @@ class Devs < Grape::API
 			requires :key, type: String, desc:"API Key"
 		end
 		put ':key', requirements: { email: /.*/ } do
-			authenticate!
-			if(current_dev.access == :read) then 
-				error!('You have no right to modify', 403) 
-			end
 			@dev = Dev.first(:api_key => params[:key])
 			if(!@dev) then
 				error!('Dev key is not found', 404)
 			end
 			@dev.update(:email => params[:email])
-			@dev.update(:dev_name => params[:dev_brand])
+			@dev.update(:dev_name => params[:dev_name])
+			@dev.update(:access => params[:access])
 			@dev.update(:updated_at => DateTime.now)
 			Dev.all(:order => [:updated_at.desc])
 		end
@@ -72,10 +75,6 @@ class Devs < Grape::API
 			requires :key, type: String, desc:"API Key"
 		end
 		delete ':key' do
-			authenticate!
-			if(current_dev.access != :all) then
-				error!('You have no right to delete a dev', 403)
-			end
 			if @dev = Dev.first(:api_key => params[:key]) then
 				@dev.destroy
 				'success'
